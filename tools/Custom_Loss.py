d36eb3237809e30f313083ab365af761134545a3
Matteo Migliarini <matteo.migliarini.01@gmail.com>
2023-10-02 17:52:03 +0200

    Fix custom loss Module

--- tools/Custom_Loss.py
+++ tools/Custom_Loss.py
@@ -3,21 +3,24 @@ from torch import Tensor
 
 
 class ReconstructionLoss(Module):
-    def __init__(self):
+    """
+    :param alpha: the weight of the KLD loss 
+    """
+    def __init__(self, alpha: float = 1):
         super(ReconstructionLoss, self).__init__()
+        self.cel = CrossEntropyLoss(reduction='mean')
+        self.kld = KLDivLoss(reduction='batchmean')
+        self.alpha = alpha
 
-    def forward(self, output_teacher: Tensor, output_student: Tensor,
-                labels: Tensor, alpha: float = 0) -> float:
+    def forward(self, output_teacher: Tensor, output_student: Tensor, labels: Tensor) -> float:
         """
         Forward pass for the reconstruction algorithm
         :param output_teacher: The output of the teacher network as a torch.Tensor
         :param output_student: The output of the student network as a torch.Tensor
         :param labels: The actual target variable as a torch.Tensor
-        :param alpha: The hyperparameter to tune
         :return: The loss value
         """
 
-        loss = CrossEntropyLoss(output_student, labels, reduction='mean') + \
-               alpha * KLDivLoss(output_teacher, output_student, reduction='batchmean')
+        loss = self.cel(output_student, labels) + self.alpha * self.kld(output_teacher, output_student)
 
         return loss
