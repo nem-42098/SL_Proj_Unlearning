68b7493c33e0e2815cf19ded7b18e520ca3c443c
Mattia Castaldo <castaldo.1837100@studenti.uniroma1.it>
2023-10-07 16:30:15 +0200

    minor changes

--- tools/Unlearner_DMM.py
+++ tools/Unlearner_DMM.py
@@ -8,7 +8,7 @@ from torch.utils.data import DataLoader
 
 
 class UnlearnerDMM:
-    def __init__(self, model: Module, device: str = 'cuda', lr: float = 1e-6, alpha: float =0.5) :
+    def __init__(self, model: Module, device: str = 'cuda', lr: float = 1e-6, alpha: float = 0.5):
         self.combined_model = model
         self.device = device
         self.lr = lr
@@ -20,78 +20,74 @@ class UnlearnerDMM:
         self.optimizer = None
         self.epoch_log = []
 
-    def forget_learn(self,forget_dataloader:DataLoader,forget_epochs:int=10):
-                
-            ### Training the forget model to get the weight: Mean estimates of the Gaussian Distribution for the Task A(Forget Dataset)
+    def forget_learn(self, forget_dataloader: DataLoader, forget_epochs: int = 10):
 
-            ### Initialise the forget model
-            self.forget_model=self.reset_weights(self.combined_model)
+        ### Training the forget model to get the weight: Mean estimates of the Gaussian Distribution for the Task A(Forget Dataset)
 
-            ### intialising the optimiser
-            self.optimizer=torch.optim.Adam(self.forget_model.parameters(), lr=self.lr)
-            ### early stopping counter
-            stop_counter=0
-           
-            ###Iterations
-            for epoch in range(forget_epochs):
-                ### iterate over the forget_dataloader
-                ### batch _loss
-                loss_epoch=0
-                
-                for i, (inputs, targets) in enumerate(forget_dataloader):
-                    ### GPU push
-                    inputs, targets = inputs.to(
+        ### Initialise the forget model
+        self.forget_model = self.reset_weights(self.combined_model)
+
+        ### intialising the optimiser
+        self.optimizer = torch.optim.Adam(self.forget_model.parameters(), lr=self.lr)
+        ### early stopping counter
+        stop_counter = 0
+
+        ###Iterations
+        for epoch in range(forget_epochs):
+            ### iterate over the forget_dataloader
+            ### batch _loss
+            loss_epoch = 0
+
+            for i, (inputs, targets) in enumerate(forget_dataloader):
+                ### GPU push
+                inputs, targets = inputs.to(
                     self.device), targets.to(self.device)
 
-                    self.forget_model=self.forget_model.to(self.device)
+                self.forget_model = self.forget_model.to(self.device)
 
-                    ### predictions from the model
-                    y_pred=self.forget_model(inputs)
+                ### predictions from the model
+                y_pred = self.forget_model(inputs)
 
-                
-                    self.optimizer.zero_grad()
+                self.optimizer.zero_grad()
 
-                    ###Calculate the Loss
-             
-                    loss = self.criterion(y_pred,targets)
-                
-                    ## Backpropogate the Loss                   
-                    loss.backward()
-                    ### Update the weights
-                    self.optimizer.step()
-                    ### Logging the measures
-                    self.log_performance(y_pred, targets, loss.item(), epoch, i, phase='Training_forget_model')
+                ###Calculate the Loss
 
-                    #### Train-set loss
-                    loss_epoch+=loss.item()
-                
+                loss = self.criterion(y_pred, targets)
 
-                
-                loss_epoch/=(i+1)
+                ## Backpropogate the Loss
+                loss.backward()
+                ### Update the weights
+                self.optimizer.step()
+                ### Logging the measures
+                self.log_performance(y_pred, targets, loss.item(), epoch, i, phase='Training_forget_model')
 
+                #### Train-set loss
+                loss_epoch += loss.item()
 
-                ### Check for early stopping: if the decrease in the loss is less than 1e-3 for straight 5 iterations
-                if (self.epoch_log[-1]-loss_epoch)<1e-3:
-                     stop_counter+=1
-                     self.epoch_log.append(loss_epoch)
+            loss_epoch /= (i + 1)
+
+            ### Check for early stopping: if the decrease in the loss is less than 1e-3 for straight 5 iterations
+            if epoch > 0:
+                if (self.epoch_log[-1] - loss_epoch) < 1e-3:
+                    stop_counter += 1
+                    self.epoch_log.append(loss_epoch)
                 else:
-                     stop_counter=0
-                     self.epoch_log.append(loss_epoch)
+                    stop_counter = 0
+                    self.epoch_log.append(loss_epoch)
 
-                if stop_counter==5:
-                     break
+                if stop_counter == 5:
+                    break
 
-    def log_performance(self, y_pred: torch.Tensor, target: torch.Tensor, loss: float, epoch: int, batch: int, phase: str) -> None:
+    def log_performance(self, y_pred: torch.Tensor, target: torch.Tensor, loss: float, epoch: int, batch: int,
+                        phase: str) -> None:
         tp = (y_pred.argmax(axis=1) == target).sum().item()
         n = target.size(0)
         self.log.append((phase, epoch, batch, tp, n, loss))
-
 
     def reset_weights(self, model: Module) -> Module:
         dumb_model = deepcopy(model)
         dumb_model = dumb_model.apply(UnlearnerDMM.init_weights)
         return dumb_model.to(self.device)
-    
 
     @staticmethod
     def init_weights(layer: Module) -> None:
@@ -109,37 +105,39 @@ class UnlearnerDMM:
         if hasattr(layer, 'bias') and layer.bias is not None:
             nn.init.zeros_(layer.bias)
         if hasattr(layer, 'weight'):
-            nn.init.xavier_normal_(layer.weight,1)    
-    
-         
-
+            nn.init.xavier_normal_(layer.weight, 1)
 
     def unlearn(self):
         ### Initialse the model for the retrained dataset
-        self.retained_Model=self.reset_weights(self.combined_model)
+        self.retained_Model = self.reset_weights(self.combined_model)
 
-        for i,(name,parameters) in enumerate(self.combined_model.named_parameters()):
-            
-            name=name.split('.')
+        for i, (name, parameters) in enumerate(self.combined_model.named_parameters()):
+
+            name = name.split('.')
             ### Feature transformation
 
-            if name[0]=='features':
+            if name[0] == 'features':
 
-                if name[2]=='weight':
-                ### computing the weights
-                    self.retained_model.features[eval(name[1])].weight=parameters*(1/(1-self.alpha))-(self.alpha/(1-self.alpha))*self.forget_model_Model.features[eval(name[1])].weight
+                if name[2] == 'weight':
+                    ### computing the weights
+                    self.retained_model.features[eval(name[1])].weight = parameters * (1 / (1 - self.alpha)) - (
+                            self.alpha / (1 - self.alpha)) * self.forget_model_Model.features[eval(name[1])].weight
                 else:
-                ### Computing the bias
-                    self.retained_model.features[eval(name[1])].bias=parameters*(1/(1-self.alpha))-(self.alpha/(1-self.alpha))*self.forget_model_Model.features[eval(name[1])].bias
+                    ### Computing the bias
+                    self.retained_model.features[eval(name[1])].bias = parameters * (1 / (1 - self.alpha)) - (
+                            self.alpha / (1 - self.alpha)) * self.forget_model_Model.features[eval(name[1])].bias
 
             ### Classifier
-            elif name[0]=='classifier':
+            elif name[0] == 'classifier':
 
-                if name[2]=='weight':
+                if name[2] == 'weight':
 
-                    self.retained_model.classifier[eval(name[1])].weight=parameters*(1/(1-self.alpha))-(self.alpha/(1-self.alpha))*self.forget_model_Model.classifier[eval(name[1])].weight
+                    self.retained_model.classifier[eval(name[1])].weight = parameters * (1 / (1 - self.alpha)) - (
+                            self.alpha / (1 - self.alpha)) * self.forget_model_Model.classifier[
+                                                                               eval(name[1])].weight
                 else:
-                
-                    self.retained_model.classifier[eval(name[1])].bias=parameters*(1/(1-self.alpha))-(self.alpha/(1-self.alpha))*self.forget_model_Model.classifier[eval(name
-                                                                                                                                                                         
-                                                                                                                                                                         [1])].bias
+
+                    self.retained_model.classifier[eval(name[1])].bias = parameters * (1 / (1 - self.alpha)) - (
+                            self.alpha / (1 - self.alpha)) * self.forget_model_Model.classifier[eval(name
+
+                                                                                                     [1])].bias
